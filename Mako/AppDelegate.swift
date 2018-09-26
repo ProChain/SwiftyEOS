6bc86dad89646dea15727dc6cc6b0eb8161d1f3a
croath <croathliu@gmail.com>
2018-09-27 00:33:44 +0800

    mako init

---
+++ Mako/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mako
+//
+//  Created by croath on 2018/9/25.
+//  Copyright © 2018 ProChain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mako/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mako
+//
+//  Created by croath on 2018/9/25.
+//  Copyright © 2018 ProChain. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
